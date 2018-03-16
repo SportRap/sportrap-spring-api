febae03b9d73b951507e9817858d13ed660b928e
Isahann Hanacleto <isahanacleto@gmail.com>
2018-03-16 20:21:42 -0300

    Alterando local da classe

--- sportrap-spring-api/src/main/java/br/com/sportrap/server/SportRapApplication.java
+++ sportrap-spring-api/src/main/java/br/com/sportrap/server/SportRapApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Server_Main {
-	
+public class SportRapApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Server_Main.class, args);
+		SpringApplication.run(SportRapApplication.class, args);
 	}
 }
