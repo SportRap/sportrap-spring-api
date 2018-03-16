2deb0921647c98b9fa4d2a1d58d7f250b2e013de
Isahann Hanacleto <isahanacleto@gmail.com>
2018-03-16 19:29:30 -0300

    Ajustes para a entidade, controller e repository de usuário

--- sportrap-spring-api/src/main/java/br/com/sportrap/api/model/Usuario.java
+++ sportrap-spring-api/src/main/java/br/com/sportrap/api/model/Usuario.java
@@ -6,71 +6,95 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
 
 import org.springframework.data.annotation.Id;
 
 import br.com.sportrap.api.model.enums.EsporteEnum;
+
 @Entity
+@Table(name = "usuario")
 public class Usuario {
-	
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private int id;
-	@Column (name = "NOME_COMPLETO")	
+
+	@Column(name = "nome_completo")
 	private String nomeCompleto;
-	@Column (name = "DATA_NASC")
-	private Date dataNascimento;
-	@Column (name = "NOME_USUARIO")
+
+	@Column(name = "nome_usuario")
 	private String nomeUsuario;
-	@Column (name = "EMAIL")
-	private String email;
-	@Column (name = "SENHA")
+
+	@Column(name = "senha")
 	private String senha;
-	
-	@Enumerated(EnumType.ORDINAL)
-	@Column (name = "ESPORTE_FAVORITO")
-	private EsporteEnum esporteFavorito;
-	
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
+
+	@Column(name = "esporte_favorito_enum")
+	@Enumerated(EnumType.STRING)
+	private EsporteEnum esporteFavoritoEnum;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNomeCompleto() {
 		return nomeCompleto;
 	}
+
 	public void setNomeCompleto(String nomeCompleto) {
 		this.nomeCompleto = nomeCompleto;
 	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
+
 	public String getNomeUsuario() {
 		return nomeUsuario;
 	}
+
 	public void setNomeUsuario(String nomeUsuario) {
 		this.nomeUsuario = nomeUsuario;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	public EsporteEnum getEsporteFavorito() {
-		return esporteFavorito;
+
+	public EsporteEnum getEsporteFavoritoEnum() {
+		return esporteFavoritoEnum;
 	}
-	public void setEsporteFavorito(EsporteEnum esporteFavorito) {
-		this.esporteFavorito = esporteFavorito;
+
+	public void setEsporteFavoritoEnum(EsporteEnum esporteFavoritoEnum) {
+		this.esporteFavoritoEnum = esporteFavoritoEnum;
 	}
 
 }
