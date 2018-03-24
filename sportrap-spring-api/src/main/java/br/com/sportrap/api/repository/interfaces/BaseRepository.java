360d801f73eb6b6b4b866d9f965ead40713a0be6
Isahann Hanacleto <isahanacleto@gmail.com>
2018-03-23 21:04:44 -0300

    Retomando repository e controller genérico

--- sportrap-spring-api/src/main/java/br/com/sportrap/api/repository/interfaces/BaseRepository.java
+++ sportrap-spring-api/src/main/java/br/com/sportrap/api/repository/interfaces/BaseRepository.java
@@ -1,11 +1,7 @@
-//package br.com.sportrap.api.repository.interfaces;
-//
-//import java.io.Serializable;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import br.com.sportrap.api.model.BaseModelo;
-//
-//public interface BaseRepository <T> extends JpaRepository<T, Long> {
-//
-//}
+package br.com.sportrap.api.repository.interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BaseRepository<T> extends JpaRepository<T, Long> {
+
+}
