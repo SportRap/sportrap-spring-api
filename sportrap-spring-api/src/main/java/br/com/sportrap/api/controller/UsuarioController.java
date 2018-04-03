f44a253f0ab0734fdaba35c070ee835c54d39fed
Isahann Hanacleto <isahanacleto@gmail.com>
2018-04-03 19:51:00 -0300

    Função genérica para envio de e-mail

--- sportrap-spring-api/src/main/java/br/com/sportrap/api/controller/UsuarioController.java
+++ sportrap-spring-api/src/main/java/br/com/sportrap/api/controller/UsuarioController.java
@@ -1,16 +1,6 @@
 package br.com.sportrap.api.controller;
 
 import java.util.List;
-import java.util.Properties;
-
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sportrap.api.model.Usuario;
+import br.com.sportrap.api.model.utils.Funcoes;
 import br.com.sportrap.api.repository.UsuarioRepository;
 
 @RestController
@@ -98,55 +89,10 @@ public class UsuarioController {
 			mensagem.append(
 					"<h5> Esta é uma mensagem automática. Não responda esse email. Caso haja dúvidas, envie um email para <i> suportesportrap@gmail.com  </i>. </h5> <br/><br/>");
 
-			return enviarEmail(emailUsuario, mensagem, "SportRap - Senha esquecida");
+			return Funcoes.enviarEmail(emailUsuario, mensagem, "SportRap - Senha esquecida");
 		} else {
 			return false;
 		}
 
-	}
-
-	private boolean enviarEmail(String emailDestinatario, StringBuilder mensagem, String assunto) {
-		String de = emailDestinatario;
-		String para = "suportesportrap@gmail.com";
-
-		Properties properties = System.getProperties();
-
-		properties.put("mail.smtp.user", "SportRap");
-		properties.put("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.port", "25");
-		properties.put("mail.debug", "true");
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.EnableSSL.enable", "true");
-
-		properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		properties.setProperty("mail.smtp.socketFactory.fallbac k", "false");
-		properties.setProperty("mail.smtp.port", "465");
-		properties.setProperty("mail.smtp.socketFactory.port", "465");
-
-		Authenticator authenticator = new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("suportesportrap@gmail.com", "sportrap123");
-			}
-		};
-
-		Session session = Session.getDefaultInstance(properties, authenticator);
-
-		try {
-			MimeMessage message = new MimeMessage(session);
-
-			message.setFrom(new InternetAddress(para));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(de));
-			message.setSubject(assunto);
-			message.setContent(mensagem.toString(), "text/html");
-
-			Transport.send(message);
-
-			System.out.println("Mensagem enviada com sucesso.");
-			return true;
-		} catch (MessagingException mex) {
-			mex.printStackTrace();
-			return false;
-		}
 	}
 }
---
+++ sportrap-spring-api/src/main/java/br/com/sportrap/api/model/utils/Funcoes.java
@@ -0,0 +1,61 @@
+package br.com.sportrap.api.model.utils;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Funcoes {
+
+	public static boolean enviarEmail(String emailDestinatario, StringBuilder mensagem, String assunto) {
+		String de = emailDestinatario;
+		String para = "suportesportrap@gmail.com";
+
+		Properties properties = System.getProperties();
+
+		properties.put("mail.smtp.user", "SportRap");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "25");
+		properties.put("mail.debug", "true");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.EnableSSL.enable", "true");
+
+		properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		properties.setProperty("mail.smtp.socketFactory.fallbac k", "false");
+		properties.setProperty("mail.smtp.port", "465");
+		properties.setProperty("mail.smtp.socketFactory.port", "465");
+
+		Authenticator authenticator = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("suportesportrap@gmail.com", "sportrap123");
+			}
+		};
+
+		Session session = Session.getDefaultInstance(properties, authenticator);
+
+		try {
+			MimeMessage message = new MimeMessage(session);
+
+			message.setFrom(new InternetAddress(para));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(de));
+			message.setSubject(assunto);
+			message.setContent(mensagem.toString(), "text/html");
+
+			Transport.send(message);
+
+			System.out.println("Mensagem enviada com sucesso.");
+			return true;
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+			return false;
+		}
+	}
+
+}
