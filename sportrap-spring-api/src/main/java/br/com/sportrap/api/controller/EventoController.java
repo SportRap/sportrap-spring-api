4231b79ec077d56b7d6d870cea347bdf9e47e783
Isahann Hanacleto <isahanacleto@gmail.com>
2018-04-03 20:31:34 -0300

    Cadastro de novo usuário convidado para evento

--- sportrap-spring-api/src/main/java/br/com/sportrap/api/controller/EventoController.java
+++ sportrap-spring-api/src/main/java/br/com/sportrap/api/controller/EventoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class EventoController {
 
 	@Autowired
 	EventoRepository eventoRepository;
-	
+
 	@Autowired
 	UsuarioRepository usuarioRepository;
 
@@ -73,47 +74,73 @@ public class EventoController {
 			eventoEscolhido.getMembrosTime2().add(usuario);
 			break;
 		}
-		
-		if(eventoRepository.save(eventoEscolhido) != null){
+
+		if (eventoRepository.save(eventoEscolhido) != null) {
 			// Usuário participando do evento
 			return true;
 		} else {
-			// Erro ao participar do evento. Os erros de evento privado ou evento já cheio devem ser tratados pelo front.
+			// Erro ao participar do evento. Os erros de evento privado ou
+			// evento já cheio devem ser tratados pelo front.
 			return false;
 		}
 
 	}
-	
+
 	@PostMapping("/convidar")
-	public boolean convidarParaEvento(@Validated @RequestBody long idEvento, 
-			@Validated @RequestBody String time,
+	public boolean convidarParaEvento(@Validated @RequestBody long idEvento, @Validated @RequestBody String time,
 			@Validated @RequestBody String email) {
 		Usuario usuarioConvidado = usuarioRepository.buscarUsuarioComEmailExistente(email);
-		
+
 		Evento eventoConvite = eventoRepository.findById(idEvento).get();
-		
-		if(usuarioConvidado == null) {
-			//usuario nao existe
+
+		if (usuarioConvidado == null) {
+			// Usuário não existente
 			String pagina = "localhost/8080";
-			String link = pagina + "//novo//" + idEvento + "//" +  time + "//" + email;
+			String link = pagina + "//novo//" + idEvento + "//" + time + "//" + email;
 			StringBuilder mensagem = new StringBuilder();
-			
+
 			mensagem.append("<h2><b> Caro Usuario, </b></h2><br/>");
-			mensagem.append(" Você foi convidado por <i>" + eventoConvite.getCriadorEvento().getNomeCompleto() + "</i>");
-			mensagem.append(" para participar do evento: " + eventoConvite.getNomeEvento() + " que será realizado em " + eventoConvite.getLocalEvento());
-			mensagem.append(" para aceitar o convite e acompanhar as notificações do mesmo, acesse o link :" + link + "e faça o seu cadastro no SportRap.");
-			
+			mensagem.append(
+					" Você foi convidado por <i>" + eventoConvite.getCriadorEvento().getNomeCompleto() + "</i>");
+			mensagem.append(" para participar do evento: " + eventoConvite.getNomeEvento() + " que será realizado em "
+					+ eventoConvite.getLocalEvento());
+			mensagem.append(" para aceitar o convite e acompanhar as notificações do mesmo, acesse o link :" + link
+					+ "e faça o seu cadastro no SportRap.");
+
 			Funcoes.enviarEmail(email, mensagem, "Você foi convidado para um evento em SportRap");
-			
+
 		} else {
-			
+
 		}
-		
+
 		return false;
 	}
-	
-	public boolean solicitarParticipacaoEvento() {
+
+	@PostMapping("/novo/{idEvento}/{time}/{emailParticipante}")
+	public boolean cadastrarNovoUsuarioConvidado(@Validated @RequestHeader(value = "idEvento") long idEvento,
+			@Validated @RequestHeader(value = "time") String time,
+			@Validated @RequestHeader(value = "emailParticipante") String emailParticipante) {
+		// Buscar se já existe o usuário pois ele pode ter recebido mais de um
+		// convite antes de estar cadastrado
+		Usuario usuarioNovo = usuarioRepository.buscarUsuarioComEmailExistente(emailParticipante);
+
+		if (usuarioNovo == null) {
+			usuarioNovo = new Usuario();
+			usuarioNovo.setEmail(emailParticipante);
+		}
+
+		if (usuarioRepository.save(usuarioNovo) != null && participarDeEvento(usuarioNovo, idEvento, time)) {
+			// Vínculo ao evento salvo com sucesso
+			return true;
+		} else {
+			// Erro ao salvar evento
 			return false;
+		}
+
+	}
+
+	public boolean solicitarParticipacaoEvento() {
+		return false;
 	}
 
 }
