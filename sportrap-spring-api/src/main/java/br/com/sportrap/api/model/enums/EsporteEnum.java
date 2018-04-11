889e23cee4ae100dab9201d6b1a36bdbff09fc70
Isahann Hanacleto <isahanacleto@gmail.com>
2018-04-10 22:07:15 -0300

    Alterando enum

--- sportrap-spring-api/src/main/java/br/com/sportrap/api/model/enums/EsporteEnum.java
+++ sportrap-spring-api/src/main/java/br/com/sportrap/api/model/enums/EsporteEnum.java
@@ -5,19 +5,29 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum EsporteEnum {
 	
-	BASQUETE("Basquete de quadra", 10),
-	FUTEBOL("Futebol", 11),
-	VOLEI_QUADRA("Vôlei de quadra",6),
-	FUTSAL("Futsal", 5),
-	TENIS_QUADRA("Tênis de quadra", 2),
-	TENIS_MESA("Tênis de mesa", 2);
+	BASQUETE("BASQUETE" ,"Basquete de quadra", 10),
+	FUTEBOL("FUTEBOL","Futebol", 11),
+	VOLEI_QUADRA("VOLEI_QUADRA","Vôlei de quadra",6),
+	FUTSAL("FUTSAL","Futsal", 5),
+	TENIS_QUADRA("TENIS_QUADRA","Tênis de quadra", 2),
+	TENIS_MESA("TENIS_MESA","Tênis de mesa", 2);
 	
-	private int jogadoresPorTime;
+	private String esporteBanco;
 	private String descricao;
-
-	private EsporteEnum(String descricao, int jogadoresPorTime) {
+	private int jogadoresPorTime;
+	
+	private EsporteEnum(String esporteBanco,String descricao, int jogadoresPorTime) {
+		this.esporteBanco = esporteBanco;
 		this.descricao = descricao;
 		this.jogadoresPorTime = jogadoresPorTime;
+	}
+	
+	public String getEsporteBanco() {
+		return esporteBanco;
+	}
+
+	public void setEsporteBanco(String esporteBanco) {
+		this.esporteBanco = esporteBanco;
 	}
 
 	public int getJogadoresPorTime() {
