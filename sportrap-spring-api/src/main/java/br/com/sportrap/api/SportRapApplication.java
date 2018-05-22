4ab96443d81f69e77d3dc2ba16d8938b212659a5
mnestor <mnestor@WS-LAB05_133.univel.univel.br>
2018-05-22 20:20:27 -0300

    Configuracao https[1]

--- sportrap-spring-api/src/main/java/br/com/sportrap/api/SportRapApplication.java
+++ sportrap-spring-api/src/main/java/br/com/sportrap/api/SportRapApplication.java
@@ -3,11 +3,25 @@ package br.com.sportrap.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+
 public class SportRapApplication {
 
+	@Configuration
+	public class WebSecurityConfigurerAdapter extends org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter {
+
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http.requiresChannel()
+	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+	      .requiresSecure();
+	  }
+	}
+	
+	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
